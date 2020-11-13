1babb3a8005fc628b187a3c43976ae2bddb366b1
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2020-11-13 12:07:37 +0000

    Scrape articles with match_id, update mergeFiles

--- src/ir/src/mergeFiles.ts
+++ src/ir/src/mergeFiles.ts
@@ -35,11 +35,13 @@ const parseFiles = async (path: string) => {
   const picksFile = await fs.readFile(path + "/picks.csv");
   const playersFile = await fs.readFile(path + "/players.csv");
   const economyFile = await fs.readFile(path + "/economy.csv");
+  const newsFile = await fs.readFile(path + "/news.csv");
 
   let picks = parse(picksFile, { columns: true });
   let players = parse(playersFile, { columns: true });
   let results = parse(resultsFile, { columns: true });
   let economy = parse(economyFile, { columns: true });
+  let news = parse(newsFile, { columns: true });
 
   results = _.groupBy(results, "match_id");
   economy = _.groupBy(economy, "match_id");
@@ -47,65 +49,74 @@ const parseFiles = async (path: string) => {
   picks = Object.fromEntries(
     picks.map(({ match_id, ...rest }: any) => [match_id, rest])
   );
+  news = Object.fromEntries(
+    news.map(({ match_id, ...rest }: any) => [match_id, rest])
+  );
 
-  return { picks, results, economy, players };
+  return { picks, results, economy, players, news };
 };
 
 const buildMatches = (
   results: any,
   economy: any,
   picks: any,
-  players: any
+  players: any,
+  news: any
 ) =>
-  Object.entries(results)
-    .map(([match_id, result]: any) => {
-      let econ = economy[match_id];
-      let matchPlayers = players[match_id];
-      let pick = picks[match_id];
-      pick = pick ? getFieldsFromPick(pick) : null;
-      matchPlayers = matchPlayers
-        ? matchPlayers.map((player: any) => getFieldsFromPlayer(player))
-        : [];
+  Object.entries(results).map(([match_id, result]: any) => {
+    let econ = economy[match_id];
+    let matchPlayers = players[match_id];
+    let pick = picks[match_id];
+    let article = news[match_id];
+    pick = pick ? getFieldsFromPick(pick) : null;
+    article = article ? article.text : "";
 
-      const maps = result.map((mapResult: any) => {
-        let mapEcon = econ
-          ? econ.find((map: any) => map._map === mapResult._map)
-          : null;
+    matchPlayers = matchPlayers
+      ? matchPlayers.map((player: any) => getFieldsFromPlayer(player))
+      : [];
 
-        const res: any = {
-          ...getFieldsFromMapResult({
-            match_id: result[0].match_id,
-            ...mapResult,
-          }),
-        };
+    const maps = result.map((mapResult: any) => {
+      let mapEcon = econ
+        ? econ.find((map: any) => map._map === mapResult._map)
+        : null;
 
-        if (mapEcon) {
-          res.economy = getFieldsFromEcon(mapEcon);
-        }
-
-        return res;
-      });
-
-      const _childDocuments_ = [...matchPlayers, ...maps];
-      if (pick) _childDocuments_.push(pick);
-
-      return {
-        ...getFieldsFromMatchResult(result[0]),
-        _childDocuments_,
+      const res: any = {
+        ...getFieldsFromMapResult({
+          match_id: result[0].match_id,
+          ...mapResult,
+        }),
       };
+
+      if (mapEcon) {
+        res.economy = getFieldsFromEcon(mapEcon);
+      }
+
+      return res;
     });
+
+    const _childDocuments_ = [...matchPlayers, ...maps];
+    if (pick) _childDocuments_.push(pick);
+
+    const res = {
+      ...getFieldsFromMatchResult(result[0]),
+      article,
+      _childDocuments_,
+    };
+
+    return res;
+  });
 
 const writeMatches = async (path: string, matches: any[]) => {
   const json = matches.map((match: any) => JSON.stringify(match)).join(",\n");
-  await fs.writeFile(path + "/output2.json", `[${json}]`);
-  console.log("Wrote output file to:", path + "/output2.json");
+  await fs.writeFile(path + "/output.json", `[${json}]`);
+  console.log("Wrote output file to:", path + "/output.json");
 };
 
 const main = async () => {
   setupProgram();
   const path = resolve(program.folder);
-  const { results, economy, picks, players } = await parseFiles(path);
-  const matches = buildMatches(results, economy, picks, players);
+  const { results, economy, picks, players, news } = await parseFiles(path);
+  const matches = buildMatches(results, economy, picks, players, news);
   writeMatches(path, matches);
 };
 
--- src/spiders/news.py
+++ src/spiders/news.py
@@ -1,64 +1,65 @@
 import scrapy
+import re
 
 class HLTVNews(scrapy.Spider):
     name = "news"
     count = 0
 
     def start_requests(self):
-        urls =[
+        urls = [
             'https://www.hltv.org/news/archive/2018/january',
-            'https://www.hltv.org/news/archive/2018/february',
-            'https://www.hltv.org/news/archive/2018/march',
-            'https://www.hltv.org/news/archive/2018/april',
-            'https://www.hltv.org/news/archive/2018/may',
-            'https://www.hltv.org/news/archive/2018/june',
-            'https://www.hltv.org/news/archive/2018/july',
-            'https://www.hltv.org/news/archive/2018/august',
-            'https://www.hltv.org/news/archive/2018/september',
-            'https://www.hltv.org/news/archive/2018/october',
-            'https://www.hltv.org/news/archive/2018/november',
-            'https://www.hltv.org/news/archive/2018/december',
-            'https://www.hltv.org/news/archive/2019/january',
-            'https://www.hltv.org/news/archive/2019/february',
-            'https://www.hltv.org/news/archive/2019/march',
-            'https://www.hltv.org/news/archive/2019/april',
-            'https://www.hltv.org/news/archive/2019/may',
-            'https://www.hltv.org/news/archive/2019/june',
-            'https://www.hltv.org/news/archive/2019/july',
-            'https://www.hltv.org/news/archive/2019/august',
-            'https://www.hltv.org/news/archive/2019/september',
-            'https://www.hltv.org/news/archive/2019/october',
-            'https://www.hltv.org/news/archive/2019/november',
-            'https://www.hltv.org/news/archive/2019/december',
-            
+            # 'https://www.hltv.org/news/archive/2018/february',
+            # 'https://www.hltv.org/news/archive/2018/march',
+            # 'https://www.hltv.org/news/archive/2018/april',
+            # 'https://www.hltv.org/news/archive/2018/may',
+            # 'https://www.hltv.org/news/archive/2018/june',
+            # 'https://www.hltv.org/news/archive/2018/july',
+            # 'https://www.hltv.org/news/archive/2018/august',
+            # 'https://www.hltv.org/news/archive/2018/september',
+            # 'https://www.hltv.org/news/archive/2018/october',
+            # 'https://www.hltv.org/news/archive/2018/november',
+            # 'https://www.hltv.org/news/archive/2018/december',
+            # 'https://www.hltv.org/news/archive/2019/january',
+            # 'https://www.hltv.org/news/archive/2019/february',
+            # 'https://www.hltv.org/news/archive/2019/march',
+            # 'https://www.hltv.org/news/archive/2019/april',
+            # 'https://www.hltv.org/news/archive/2019/may',
+            # 'https://www.hltv.org/news/archive/2019/june',
+            # 'https://www.hltv.org/news/archive/2019/july',
+            # 'https://www.hltv.org/news/archive/2019/august',
+            # 'https://www.hltv.org/news/archive/2019/september',
+            # 'https://www.hltv.org/news/archive/2019/october',
+            # 'https://www.hltv.org/news/archive/2019/november',
+            # 'https://www.hltv.org/news/archive/2019/december',
+
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse_search_page)
-    
-    def parse_search_page(self, response):
-        """ url_split = response.url.split("/")
-        month = url_split[-1]
-        year = url_split[-2]
-        filename = 'news-%s-%s.html' % (month, year)
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename) """
 
+    def parse_search_page(self, response):
         for link in response.css('a.article'):
             next_article = link.css('a::attr(href)').get()
             next_article = response.urljoin(next_article)
             yield scrapy.Request(next_article, callback=self.parse_article)
 
-
     def parse_article(self, response):
         self.count += 1
-        text = response.css('div.newsdsl .legacy-con p > span::text, div.newsdsl .legacy-con p > span > a::text').getall()
+        text = response.css(
+            'div.newsdsl .legacy-con p > span::text, div.newsdsl .legacy-con p > span > a::text').getall()
+        match_link = response.css('.newsitem-match-result-score-con > a')
+        match_id = ""
+        if not match_link == []:
+            p = re.compile('\/matches\/(\d*)\/')
+            m = p.match(match_link.attrib["href"])
+            match_id = m.groups()[0]
+    
         collapsed_text = ' '.join(''.join(text).split())
+        
         yield {
             'ID': self.count - 1,
             'date': response.css('div.article-info .date::text').get(),
             'title': response.css('h1.headline::text').get(),
             'text': collapsed_text,
             'author': response.css('div.article-info .author a span::text').get(),
+            'match_id': match_id
         }
-
